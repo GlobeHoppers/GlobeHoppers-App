e1eeff58e874726de2be38c40728da9a346936d5
jonathanjoelneptune <jonathanjoelneptune@gmail.com>
2026-07-06 08:17:52 -0700

    Update vite.config.js

--- journeylines/vite.config.js
+++ journeylines/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// For GitHub Pages project sites, set base to '/REPO_NAME/' after you create the repo.
-// For local dev or user/org root sites, '/' is correct.
 export default defineConfig({
   plugins: [react()],
-  base: './'
-});
+  base: '/JourneyLines/'
+})
